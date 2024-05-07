bdfe0668a0c6abf627a676a3325e6644e1cf3815
mr-minty <167585646+mr-minty@users.noreply.github.com>
2024-05-07 15:38:35 -0700

    Update UserdataScript-phase-3.sh

--- UserdataScript-phase-3.sh
+++ UserdataScript-phase-3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -xe
 apt update -y
 apt install nodejs unzip wget npm mysql-client -y
-#wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-200-ACCAP1-1-DEV/code.zip -P /home/ubuntu
 wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-200-ACCAP1-1-79581/1-lab-capstone-project-1/code.zip -P /home/ubuntu
 cd /home/ubuntu
 unzip code.zip -x "resources/codebase_partner/node_modules/*"
